6a8914967135bafc0f25823328aea1ed7a85d8e1
mohdshanu12 <129376678+mohdshanu12@users.noreply.github.com>
2023-03-30 15:04:57 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -2,3 +2,14 @@ resource "local_file" "File_block" {
   filename = "Hello.py"
   content  = "Hello world"
 }
+resource "local_file" "file_block1"{
+  filename="hello1.py"
+  content=local.con
+}
+locals{
+  con="<p> Hello world <p>"
+}
+resource "random_id" "random_value"{
+    byte_length=8
+
+}
